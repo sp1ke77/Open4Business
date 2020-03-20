d116a2008e1cac47670e0b8ea5f927a90645a12b
snake <francisco.soares@outdare.pt>
2020-03-20 16:04:22 +0000

    Add resources and Layout fix

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -9,14 +9,18 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- CSS -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{asset('dropzone-5.7.0/dist/min/dropzone.min.css')}}">
-
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css">
+        <link rel="stylesheet" type="text/css" href="{{url('css/website.css')}}">
         <!-- JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="{{asset('dropzone-5.7.0/dist/min/dropzone.min.js')}}" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox-plus-jquery.min.js"></script>
     </head>
     <body>
 
-        <div class="container">
+        <div class="website container">
             @yield('content')
         </div>
     </body>
--- resources/views/submission/company_files.blade.php
+++ resources/views/submission/company_files.blade.php
@@ -4,18 +4,33 @@
 
 @section('content')
 <div class="open4biz-header">
-    <h1>VOST Open4Business</h1>
+    
+    <div class="row">
+        <div class="col-sm">
+        <img src="https://info.vost.pt/wp-content/uploads/2019/08/cropped-TEMPWEBSITE_ELEMENTS_VOSTPTLOGO_w160px-1.png">
+        </div>
+        <div class="col-sm header-title">
+        <h1>Open4Business</h1>
+        </div>
+        <div class="col-sm">
+        &nbsp;
+        </div>
+        <div class="col-sm">
+        &nbsp;
+        </div>
+    </div>
+
 </div>
 <div class='content'>
     <p>Este formulário destina-se à submissão de um ficheiro com dados dos vários equipamentos abertos e seus horários. Para fazer download do ficheiro modelo a preencher, <a href="/examples/upload.csv">clique aqui</a>.
     O ficheiro deverá conter uma linha por cada horário de cada equipamento. Para melhor esclarecer a forma correta de preenchimento veja a seguinte imagem:<p>
-    <img src="{{url('examples/uploadcsv.png')}}">
+    <a href="{{url('examples/uploadcsv.png')}}" data-lightbox="image-csvexample" data-title="exemplo csv"><img width="100%" src="{{url('examples/uploadcsv.png')}}"></a>
     <!-- Dropzone -->
-    <h3>Faça aqui upload do ficheiro CSV e do logótipo da Marca</h3>
+    <h3>1) Faça aqui upload do ficheiro CSV e do logótipo da Marca</h3>
     <form action="{{route('company.infoupload')}}" class='dropzone' >
 
     </form> 
-    <h3>Contacto responsável</h3>
+    <h3>2) Contacto responsável</h3>
     @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -27,11 +42,17 @@
 @endif
     <form action="{{route('company.submitform')}}" class='dropzone' method="POST">
     <input type="hidden" name = "_token" value="{{ csrf_token() }}">
-    <label for="nome">Nome: </label><input type="text" name = "nome" value="{{old('nome')}}  ">
-    <label for="telefone">Telefone: </label><input type="text" name = "telefone" value="{{old('telefone')}}">
-    <label for="email">Email: </label><input type="text" name = "email" value="{{old('email')}}">
-    <button type="submit" name="submit">submit</button>
-    </form> 
+    <div class="form-group">
+        <label for="nome">Nome: </label><input class="form-control" type="text" name = "nome" value="{{old('nome')}}  ">
+    </div>
+    <div class="form-group">
+        <label for="telefone">Telefone: </label><input class="form-control" type="text" name = "telefone" value="{{old('telefone')}}">
+    </div>
+    <div class="form-group">
+        <label for="email">Email: </label><input class="form-control" type="text" name = "email" value="{{old('email')}}">
+    </div>
+    <small id="submitHelp" class="form-text text-muted">Antes de submeter, verifique por favor que introduziu o logótipo da empresa e um ficheiro CSV.</small>
+    <button type="submit" name="submit" class="btn btn-primary">Enviar</button>
 </div>
 
 @if(session()->has('success_message'))
