8805ea0ddfbbd56ee44023eb79781bde1bc17f62
Mário Santos <eu@mariosantos.net>
2020-03-28 16:29:54 +0000

    Add Theme from Invision

--- public/js/app.js
+++ public/js/app.js
@@ -41544,8 +41544,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Projectos\VOST\Open4Business\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Projectos\VOST\Open4Business\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/mariosantos/code/vost/Open4Business/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/mariosantos/code/vost/Open4Business/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -10,16 +10,53 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('css/website.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/website.css') }}">
     <!-- JS -->
 </head>
 
 <body>
+    <header class="container">
+        <h1 class="logo">
+            <a href="/">
+                <span>#open</span>
+                4businesspt
+            </a>
+        </h1>
+    </header>
+    <main class="">
+        <div class="main__alert" id="cookie-alert">
+            <small class="container">
+                Utilizamos cookies para melhorar a experiência de navegação. Sabe mais sobre a nossa política de cookies <a href="#">aqui</a>. <a href="#" class="icon icon--close">&times;</a>
+            </small>
+        </div>
 
-    <div class="website container">
-        @yield('content')
-    </div>
+        <div class="section--primary mb-5">
+            <div class="container">
+                <p class="lead">Informa aqui que a tua empresa e/ou negócio continua aberta e como é que <wbr> podemos usufruir dos produtos e serviços neste período excecional.</p>
+                <div class="button__wrapper">
+                    <a href="" class="button button--secondary">Pequenas e médias empresas</a>
+                    <a href="" class="button button--secondary">Grandes empresas e cadeias</a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            @yield('content')
+        </div>
+    </main>
+    <section class="section--primary section__cta">
+        <div class="container">
+            <div class="row">
+                <div class="col-3 section__cta__hash">
+                    #open4businesspt
+                </div>
+                <div class="col-9 section__cta__info">
+                    Partilhe esta hashtag e este site com os negócios locais para que possam incluir a sua atividade, disponibilidade e modo de funcionamento nesta altura excecional.
+
+                </div>
+            </div>
+        </div>
+    </section>
     <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     @yield('script')
 </body>
