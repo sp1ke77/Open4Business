70550d87a06b19095547e635b70ce2c7f34a2c32
Miguel Santos <github@miguelsantos.tech>
2020-04-02 22:09:56 +0100

    fix: migration typo

--- database/migrations/2020_04_02_144042_add_appointment_columns_to_business_schedules.php
+++ database/migrations/2020_04_02_144042_add_appointment_columns_to_business_schedules.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAppointmentCollumnsToBusinessSchedules extends Migration
+class AddAppointmentColumnsToBusinessSchedules extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +15,7 @@ class AddAppointmentCollumnsToBusinessSchedules extends Migration
     public function up()
     {
         Schema::table('business_schedules', function (Blueprint $table) {
-            $table->boolean("by_appointment")->default(false);
+            $table->boolean('by_appointment')->default(false);
             $table->string('by_appointment_contacts')->nullable();
         });
     }
@@ -27,8 +28,8 @@ class AddAppointmentCollumnsToBusinessSchedules extends Migration
     public function down()
     {
         Schema::table('business_schedules', function (Blueprint $table) {
-            $table->dropColumn("by_appointment");
-            $table->dropColumn("by_appointment_contacts");
+            $table->dropColumn('by_appointment');
+            $table->dropColumn('by_appointment_contacts');
         });
     }
 }
