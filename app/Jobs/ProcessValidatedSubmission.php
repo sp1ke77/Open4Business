8da3bf4859ae1f93f810b0ff9a7ec3eb230fe96a
Miguel Santos <github@miguelsantos.tech>
2020-04-03 21:24:47 +0100

    fix: Submission Validation

--- app/Jobs/ProcessValidatedSubmission.php
+++ app/Jobs/ProcessValidatedSubmission.php
@@ -7,7 +7,6 @@ use App\Business;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Http\FileHelpers;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
@@ -37,20 +36,20 @@ class ProcessValidatedSubmission implements ShouldQueue
     {
         foreach ($this->submission->entries as $entry) {
             if ($entry->business_id == null) {
-                $business = Business::createBusiness($entry->company,$entry->store_name, $entry->address, $entry->parish, $entry->county, $entry->district, $entry->postal_code, $entry->lat, $entry->long, $entry->phone_number, $entry->sector, $this->submission->firstname, $this->submission->lastname, $this->submission->contact, $this->submission->email, $this->submission->user_id);
+                $business = Business::createBusiness($entry->company, $entry->store_name, $entry->address, $entry->parish, $entry->county, $entry->district, $entry->postal_code, $entry->lat, $entry->long, $entry->phone_number, $entry->sector, $this->submission->firstname, $this->submission->lastname, $this->submission->contact, $this->submission->email, $this->submission->user_id);
                 $entry->updateBusiness($business->id);
             } else {
                 $entry->business->removeSchedules();
                 $entry->business->setOwner($this->submission->user_id);
                 $entry->business->updateContactInformation($this->submission->firstname, $this->submission->lastname, $this->submission->contact, $this->submission->email);
-                $entry->business->updateStoreInformation($entry->company,$entry->store_name, $entry->address, $entry->parish, $entry->county, $entry->district, $entry->postal_code, $entry->lat, $entry->long, $entry->phone_number, $entry->sector);
+                $entry->business->updateStoreInformation($entry->company, $entry->store_name, $entry->address, $entry->parish, $entry->county, $entry->district, $entry->postal_code, $entry->lat, $entry->long, $entry->phone_number, $entry->sector);
             }
             foreach ($entry->schedules as $schedule) {
-                $entry->business->addSchedule($schedule->start_hour, $schedule->end_hour, $schedule->sunday, $schedule->monday, $schedule->tuesday, $schedule->wednesday, $schedule->thrusday, $schedule->friday, $schedule->saturday, $schedule->type, $schedule->section_of_day);
+                $entry->business->addSchedule($schedule->start_hour, $schedule->end_hour, $schedule->sunday, $schedule->monday, $schedule->tuesday, $schedule->wednesday, $schedule->thrusday, $schedule->friday, $schedule->saturday, $schedule->type, $schedule->section_of_day, $schedule->by_appointment, $schedule->by_appointment_contacts);
             }
-            $img_file = Storage::disk('public_submissions')->get($this->submission->image);
+            $img_file           = Storage::disk('public_submissions')->get($this->submission->image);
             $img_file_extension = \pathinfo($this->submission->image, PATHINFO_EXTENSION);
-            Storage::disk('public_businesses')->put($entry->business_id.'.'.$img_file_extension, $img_file); 
+            Storage::disk('public_businesses')->put($entry->business_id.'.'.$img_file_extension, $img_file);
         }
     }
 }
